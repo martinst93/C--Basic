8fbdb8b0dd81f1718010666b8f76597bc33b4171
martinst93 <stojanov003@gmail.com>
2021-03-23 14:10:17 +0100

    Arrow Cosole App

---
+++ 01.Arrow/Arrow-Solution/Arrow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("  *    *");
+            Console.WriteLine("   *    *                                   *");
+            Console.WriteLine("    *    *                                    *");
+            Console.WriteLine("    ********************************************");
+            Console.WriteLine("    *    *                                    *");
+            Console.WriteLine("   *    *                                   *");
+            Console.WriteLine("  *    *");
+
+            Console.ReadLine();
+        }
+    }
+}
