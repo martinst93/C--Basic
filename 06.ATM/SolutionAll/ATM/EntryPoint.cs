587ba0baea71775817186b99b983eb4bd2b2fb7c
martinst93 <stojanov003@gmail.com>
2021-04-07 15:28:35 +0200

    ATM

--- 06.ATM/SolutionAll/ATM/EntryPoint.cs
+++ 06.ATM/SolutionAll/ATM/EntryPoint.cs
@@ -3,12 +3,10 @@ using System;
 
 namespace ATM
 {
-    class EntryPoint
+    internal class EntryPoint
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            
-            
             Customers[] customer = new Customers[4]
              {
                 new Customers("Ken Pen", "1234-1234-1234", 1234, 500),
@@ -16,7 +14,7 @@ namespace ATM
                 new Customers("Bill Smith", "5555-5555-5555", 5555, 700),
                 new Customers("Howard Stern", "7777-7777-7777", 7777, 800),
              };
-           
+
             Console.WriteLine("Enter your card number");
             string cardNumberInput = Console.ReadLine();
             Console.WriteLine("Enter your PIN number");
@@ -28,18 +26,15 @@ namespace ATM
             int pinConverted = Int32.Parse(pinNumber);
             int convertedOperation = Int32.Parse(operation);
 
-            
             Validation(customer, cardNumberInput, pinConverted, convertedOperation);
 
-            
             Console.ReadLine();
         }
 
-        static void Validation(Customers[] customer, string cardNumberInput, int pinConverted, int convertedOperation)
+        private static void Validation(Customers[] customer, string cardNumberInput, int pinConverted, int convertedOperation)
         {
             foreach (var item in customer)
             {
-                
                 if (cardNumberInput == item.CardNumber && pinConverted == item.PinNumber)
                 {
                     Console.WriteLine($"Welcome {item.Name}!");
@@ -49,32 +44,33 @@ namespace ATM
                         case 1:
                             BalanceChecking(customer, cardNumberInput);
                             break;
+
                         case 2:
                             CashWithdrawal(customer, cardNumberInput);
                             break;
+
                         case 3:
                             CashDeposition(customer, cardNumberInput);
                             break;
+
                         default:
                             Console.WriteLine("---------");
                             break;
                     }
                 }
-            
             }
-
         }
 
-        static void BalanceChecking(Customers[] customer, string creditCard)
+        private static void BalanceChecking(Customers[] customer, string creditCard)
         {
-            for (var i=0; i<customer.Length; i++)
+            for (var i = 0; i < customer.Length; i++)
             {
-                if(customer[i].CardNumber == creditCard)
-                Console.WriteLine($"The Balance of your account is: ${customer[i].Balance}" );
+                if (customer[i].CardNumber == creditCard)
+                    Console.WriteLine($"The Balance of your account is: ${customer[i].Balance}");
             }
         }
 
-        static void CashWithdrawal(Customers[] customer, string cardNumberInput)
+        private static void CashWithdrawal(Customers[] customer, string cardNumberInput)
         {
             Console.WriteLine("Enter the amount of money you wish to withdraw");
             string withdrawAmountInput = Console.ReadLine();
@@ -82,26 +78,25 @@ namespace ATM
             int con = Int32.Parse(withdrawAmountInput);
             int res;
 
-                foreach (var item in customer)
+            foreach (var item in customer)
+            {
+                if (item.CardNumber == cardNumberInput)
                 {
-                        if(item.CardNumber == cardNumberInput)
+                    if (con <= item.Balance)
                     {
-                        if (con <= item.Balance) 
-                        {
-                            res = item.Balance - con;
-                            Console.WriteLine($"you have withdrawn ${con}");
-                            Console.WriteLine($"You have ${res} left in your account balance");
-                        }
-                        else
-                        {
-                            Console.WriteLine($"You don't have enough cash in your account!");
-                        }
+                        res = item.Balance - con;
+                        Console.WriteLine($"you have withdrawn ${con}");
+                        Console.WriteLine($"You have ${res} left in your account balance");
                     }
-                        
+                    else
+                    {
+                        Console.WriteLine($"You don't have enough cash in your account!");
+                    }
                 }
+            }
         }
 
-        static void CashDeposition(Customers[] customer, string cardNumberInput)
+        private static void CashDeposition(Customers[] customer, string cardNumberInput)
         {
             Console.WriteLine("Enter the amount of money you wish to deposit");
             string depositInput = Console.ReadLine();
@@ -110,14 +105,12 @@ namespace ATM
             int res;
             foreach (var item in customer)
             {
-                
-                    if (cardNumberInput == item.CardNumber)
+                if (cardNumberInput == item.CardNumber)
                 {
                     Console.WriteLine($"You have deposited ${con} into your account!");
                     res = item.Balance + con;
                     Console.WriteLine($"You have ${res} in your account!");
                 }
-                
             }
         }
     }
